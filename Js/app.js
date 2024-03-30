e97df7ec76991b51651532a032bcecfe87329fc3
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-30 16:03:05 +0100

    EDIT: checkbox layout

--- Js/app.js
+++ Js/app.js
@@ -1,12 +1,10 @@
-console.log('Console test');
-
 const backEndHourPrice = 20.50;
 const frontEndHourPrice = 15.30;
 const ProjectAnalysisHourPrice = 33.60;
 const projectHours = 10;
 const formElement = document.getElementById('form');
-const priceBoldElement = document.getElementById ('price-bold');
-const priceNormalElement = document.getElementById ('price-normal');
+const priceBoldElement = document.getElementById('price-bold');
+const priceNormalElement = document.getElementById('price-normal');
 const discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 const discountSale = 0.25;
 const forms = document.querySelectorAll('.needs-validation');
@@ -14,14 +12,10 @@ formElement.addEventListener('submit', jobPricing);
 
 function jobPricing(e) {
     e.preventDefault()
-
     const jobSelectionElement = document.getElementById('job-selection');
     const discountElement = document.getElementById('discount');
     let discount = discountElement.value;
     const selectedJob = jobSelectionElement.value;
-
-    console.log(selectedJob);
-    
     let jobPrice = 0;
     if (selectedJob === 'backend') {
         jobPrice = 20.50;
@@ -31,40 +25,22 @@ function jobPricing(e) {
         jobPrice = 33.60;
     }
 
-    console.log('price', jobPrice);
-
-    let find = discountCode.includes(discount);
-    
     let priceDiscount = 0;
-
+    let find = discountCode.includes(discount);
     console.log(find);
     if (find) {
         jobPrice = jobPrice - (jobPrice * discountSale);
-    } else if (discount == '')  { 
-        
+    } else if (discount == '') {
+
     } else if (!find) {
         alert('Il codice inserito non è valido');
 
     }
-    console.log(priceDiscount);
 
     const estimatePrice = Intl.NumberFormat('en-DE', { style: 'currency', currency: 'EUR' }).format(projectHours * jobPrice);
-
-    console.log(estimatePrice);
-
     let price = estimatePrice.split('.');
-    
-    
-    console.log(price);
-    console.log(discountCode);
-    console.log(discount);
-
-  
-
-    
-
-   priceBoldElement.innerHTML = price[0];
-   priceNormalElement.innerHTML = (',' + price[1]);
+    priceBoldElement.innerHTML = price[0];
+    priceNormalElement.innerHTML = (',' + price[1]);
 
 }
 
