5771c34a999dd60bb1f00218f51880986bb4730e
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-04-02 18:37:51 +0200

    EDIT : footer link

--- Js/app.js
+++ Js/app.js
@@ -1,28 +1,27 @@
+const projectHours = 10;
 const backEndHourPrice = 20.50;
 const frontEndHourPrice = 15.30;
 const ProjectAnalysisHourPrice = 33.60;
-const projectHours = 10;
 const formElement = document.getElementById('form');
 const priceBoldElement = document.getElementById('price-bold');
 const priceNormalElement = document.getElementById('price-normal');
+const jobSelectionElement = document.getElementById('job-selection');
+const discountElement = document.getElementById('discount');
 const discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 const discountSale = 0.25;
-const forms = document.querySelectorAll('.needs-validation');
 formElement.addEventListener('submit', jobPricing);
 
 function jobPricing(e) {
     e.preventDefault()
-    const jobSelectionElement = document.getElementById('job-selection');
-    const discountElement = document.getElementById('discount');
     let discount = discountElement.value;
     const selectedJob = jobSelectionElement.value;
     let jobPrice = 0;
     if (selectedJob === 'backend') {
-        jobPrice = 20.50;
+        jobPrice = backEndHourPrice;
     } else if (selectedJob === 'frontend') {
-        jobPrice = 15.30;
+        jobPrice = frontEndHourPrice;
     } else if (selectedJob === 'project') {
-        jobPrice = 33.60;
+        jobPrice = ProjectAnalysisHourPrice;
     }
 
     let priceDiscount = 0;
