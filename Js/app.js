7143fc28dae34987b137ffd2595a153d55ba08d3
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-04-06 17:27:58 +0200

    EDIT : js inline style | EDIT : fav-ico | Cleaning code | Editing files

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,7 @@
+//console.log('CONSOLE TEST');
+
+// global var
+
 const projectHours = 10;
 const backEndHourPrice = 20.50;
 const frontEndHourPrice = 15.30;
@@ -7,8 +11,11 @@ const priceBoldElement = document.getElementById('price-bold');
 const priceNormalElement = document.getElementById('price-normal');
 const jobSelectionElement = document.getElementById('job-selection');
 const discountElement = document.getElementById('discount');
-const discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+const invalidMessageElement = document.getElementById('invalid-message');
 const discountSale = 0.25;
+const discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+
+// main validation functiom
 
 (() => {
     const forms = document.querySelectorAll('.needs-validation')
@@ -24,10 +31,18 @@ const discountSale = 0.25;
     })
 })()
 
+// job price calculator 
+
 function jobPricing(discountElement, jobSelectionElement, backEndHourPrice, frontEndHourPrice, ProjectAnalysisHourPrice, discountCode) {
-    
+
     let discount = discountElement.value;
+
+    //console.log('codice sconto :  ' ,discount);
+
     const selectedJob = jobSelectionElement.value;
+
+    //console.log('tipo lavoro : ' , selectedJob);
+
     let jobPrice = 0;
     if (selectedJob === 'backend') {
         jobPrice = backEndHourPrice;
@@ -40,16 +55,22 @@ function jobPricing(discountElement, jobSelectionElement, backEndHourPrice, fron
     let priceDiscount = 0;
     let find = discountCode.includes(discount);
     if (find) {
+        discountElement.setAttribute('style', 'color:green; border-color:green;');
         jobPrice = jobPrice - (jobPrice * discountSale);
+
     } else if (discount == '') {
-        
+
     } else if (!find) {
-        alert('Il codice inserito non è valido');
-        
+        discountElement.setAttribute('style', 'color:red; border-color:red;');
+        invalidMessageElement.innerHTML = ('Il codice selezionato non è valido')
+
     }
+
+    //console.log('prezzo : ', jobPrice);
 
     const estimatePrice = Intl.NumberFormat('en-DE', { style: 'currency', currency: 'EUR' }).format(projectHours * jobPrice);
     let price = estimatePrice.split('.');
+    //console.log('prezzo : ' , price);
     priceBoldElement.innerHTML = price[0];
     priceNormalElement.innerHTML = (',' + price[1]);
 }
